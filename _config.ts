ec80ef8a5a06c3e63ba43d99904a249709ab445d
ooker <ganuongphap@gmail.com>
2024-08-07 20:44:34 +0700

    chore: thử đổi location trong config

--- _config.ts
+++ _config.ts
@@ -3,7 +3,9 @@ import wiki from "wiki/mod.ts";
 
 const site = lume({
   src: "./docs",
+  location: new URL("https://quacau-thesphere.github.io/GW-vat-ly-an-du-trong-triet-hoc/"),
 });
+
 site.use(wiki());
 
 export default site;
