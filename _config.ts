a679fd988f81f67ba7f54f4ce53b576f654f2ef3
ooker <ganuongphap@gmail.com>
2024-08-07 20:22:08 +0700

    chore: xoá location trong config

--- _config.ts
+++ _config.ts
@@ -1,9 +1,7 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
 
-const site = lume({
-  location: new URL("https://quacau-thesphere.github.io/GW-vat-ly-an-du-trong-triet-hoc/"),
-});
+const site = lume();
 
 site.use(wiki());
 
