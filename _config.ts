a044a8dcaa728e188e436288b6b5bdd7bd0653a3
ooker <ganuongphap@gmail.com>
2024-08-08 14:25:23 +0700

    backup trước khi debug favicon

--- _config.ts
+++ _config.ts
@@ -1,10 +1,16 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
+import favicon from "lume/plugins/favicon.ts";
 
 const site = lume({
   src: "./docs",
-  location: new URL("https://quacau-thesphere.github.io/GW-vat-ly-an-du-trong-triet-hoc/"),
+  // location: new URL("https://quacau-thesphere.github.io/GW-vat-ly-an-du-trong-triet-hoc/"),
 });
+
+site.use(favicon({
+  // input: "/favicon.svg",
+  input: "/assets/favicon.svg",
+}));
 
 site.use(wiki());
 
