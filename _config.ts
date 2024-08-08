86d30ba9343669e1ddd8f6fabccdd276d6fe551f
ooker <ganuongphap@gmail.com>
2024-08-08 23:28:37 +0700

    bug: og image not working

--- _config.ts
+++ _config.ts
@@ -1,17 +1,19 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
-import favicon from "lume/plugins/favicon.ts";
+import metas from "lume/plugins/metas.ts";
+import ogImages from "lume/plugins/og_images.ts";
 
 const site = lume({
   src: "./docs",
-  // location: new URL("https://quacau-thesphere.github.io/GW-vat-ly-an-du-trong-triet-hoc/"),
 });
 
-site.use(favicon({
-  // input: "/favicon.svg",
-  input: "/assets/favicon.svg",
+site.use(wiki({
+  favicon: {
+    input: "assets/favicon.svg"
+  }
 }));
+site.use(metas());
+site.use(ogImages());
 
-site.use(wiki());
 
 export default site;
