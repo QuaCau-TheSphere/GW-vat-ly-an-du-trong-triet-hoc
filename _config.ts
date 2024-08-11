9fd3ddea991167db4e437037b4d702694d11fa5f
ooker <ganuongphap@gmail.com>
2024-08-11 19:03:40 +0700

    feat: thêm Google Analytics và Mermaid

--- _config.ts
+++ _config.ts
@@ -3,13 +3,19 @@ import wiki from "wiki/mod.ts";
 import metas from "lume/plugins/metas.ts";
 import ogImages from "lume/plugins/og_images.ts";
 
+const markdown = {
+  options: {
+    breaks: true,
+  },
+};
+
 const site = lume({
   src: "./docs",
   location: new URL("https://vậtlý.quảcầu.cc"),
   server: {
     open: true,
   },
-});
+}, { markdown });
 
 site.use(wiki({
   favicon: {
